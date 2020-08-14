0a247cb9149470e19260b93bea1ba3762d778558
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-08-14 18:08:08 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -6,7 +6,7 @@ from platform import python_version, uname
 from shutil import which
 from os import remove
 from telethon import version
-from userbot import bot, CMD_HELP, ALIVE_NAME, UPSTREAM_REPO_BRANCH, ALIVE_LOGO, USERBOT_VERSION, IMG, StartTime, 
+from userbot import bot, CMD_HELP, ALIVE_NAME, UPSTREAM_REPO_BRANCH, ALIVE_LOGO, USERBOT_VERSION, IMG, StartTime 
 from userbot.events import register
 from datetime import datetime
 import psutil
