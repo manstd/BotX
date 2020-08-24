d877647ada5f7a47df6d48012fcf090806f5bb06
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-08-24 19:53:04 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -31,4 +31,4 @@ async def help(event):
         for i in CMD_HELP:
             string += "`" + str(i)
             string += "`\t•\t "
-        await event.reply(`string`")
+        await event.reply(`string`)
